443897f5c2ba461b72071afe24a5b68377ef3823
pranotobudi <pranotobudi.s@gmail.com>
2018-12-22 17:25:13 +0700

    Home and About page

--- hello_wolrd/urls.py
+++ hello_wolrd/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')), # new
+    path('about/', include('pages.urls')), # new
 ]
